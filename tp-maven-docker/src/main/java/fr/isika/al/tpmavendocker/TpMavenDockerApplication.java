edc58ee523549fcd53c3c85dfcf8ed4a6f035523
ClementGuio <clementguio@gmail.com>
2024-03-07 11:09:22 +0100

    Création du projet

---
+++ tp-maven-docker/src/main/java/fr/isika/al/tpmavendocker/TpMavenDockerApplication.java
@@ -0,0 +1,13 @@
+package fr.isika.al.tpmavendocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpMavenDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpMavenDockerApplication.class, args);
+	}
+
+}
