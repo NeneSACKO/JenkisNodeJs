2ca5e53370ec0cc6917f9a39ea6bba11506c61f9
SADEQ SABRINE <sabrine.sadeq@gmail.com>
2024-03-07 14:55:05 +0100

    view

--- tp-maven-docker/src/main/java/fr/isika/al/tpmavendocker/controller/HelloController.java
+++ tp-maven-docker/src/main/java/fr/isika/al/tpmavendocker/controller/HelloController.java
@@ -1,12 +1,16 @@
 package fr.isika.al.tpmavendocker.controller;
 
+import org.apache.logging.log4j.message.Message;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class HelloController {
 	
 	@GetMapping("/")
@@ -19,4 +23,5 @@ public class HelloController {
 //		String name =(String) model.getAttribute("name");
 //		return "/";
 //	}
+	
 }
